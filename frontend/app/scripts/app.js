b05f5fa8ef16557d4892c8b8306bc93b04f856b6
Igor Serpak <igor.serpak@gmail.com>
2015-06-07 10:04:48 -0300

    Initial commit

---
+++ frontend/app/scripts/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name frontendApp
+ * @description
+ * # frontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('frontendApp', [
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
